7f323f12978b52f6db4ca037924cb3ff60195335
Aditya pareek <apareek@planful.com>
2025-04-29 13:26:25 +0530

    -

--- src/index.js
+++ src/index.js
@@ -13,11 +13,11 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 const store = configureStore({
   reducer: rootReducer,
 });
-
+const clientId = "455757362263-intk7c3aipgm7m95migdhkcbcc0de3eu.apps.googleusercontent.com"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <Provider store={store}>
-      <GoogleOAuthProvider clientId={"455757362263-intk7c3aipgm7m95migdhkcbcc0de3eu.apps.googleusercontent.com"}>
+      <GoogleOAuthProvider clientId={clientId}>
         <BrowserRouter>
           <App />
           <Toaster />
